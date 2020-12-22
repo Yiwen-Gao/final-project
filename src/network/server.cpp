377a44ff58232837765de438e08743e8cb9b0378
jul-gold <julian.goldberg@columbia.edu>
2020-12-22 12:51:41 -0500

    Add namespaces to Server.cpp

--- src/network/server.cpp
+++ src/network/server.cpp
@@ -7,6 +7,45 @@
 
 using namespace std;
 
+#define STACK (1024*1024)
+static char mstack[STACK];
+static char pstack[STACK];
+static char cstack[STACK];
+
+int mpipe[2][2];
+int ppipe[2][2];
+int cpipe[2][2];
+
+void setup_spaces(){
+  int flags = CLONE_NEWPID | CLONE_NEWNET | CLONE_NEWUTS | CLONE_NEWUSER | SIG_CHLD;
+
+  pipe(mpipe[0]);
+  pipe(mpipe[1]);
+  if(clone(mail_exec, mstack+STACK, flags, &mpipe)<0){
+      perror("failed to clone");
+  }
+  close(mpipe[0][1]);
+  close(mpipe[1][0]);
+
+  pipe(ppipe[0]);
+  pipe(ppipe[1]);
+  if(clone(password_exec, pstack+STACK, flags, &ppipe)<0){
+      perror("failed to clone");
+  }
+  close(ppipe[0][1]);
+  close(ppipe[1][0]);
+
+  pipe(cpipe[0]);
+  pipe(cpipe[1]);
+  if(clone(ca_exec, cstack+STACK, flags, &cpipe)<0){
+      perror("failed to clone");
+  }
+  close(cpipe[0][1]);
+  close(cpipe[1][0]);
+}
+
+
+
 // void getcert(string username, string password, string csr_path) {
     // verify username and password
     // int status;
@@ -47,6 +86,7 @@ int main(int argc, char **argv) {
     //     perror("usage: ./server <ca_cert> <server_cert> <server_key>");
     //     exit(1);
     // }
+    setup_spaces();
 
     const char *CA_CERT = "../../server/certificates/ca/certs/ca.cert.pem"; // *(++argv);
     const char *SERVER_CERT = "../../server/certificates/ca/intermediate/certs/localhost.cert.pem"; // *(++argv);
