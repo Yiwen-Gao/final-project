f119094db210f924b89addd99706dd30865deb97
Yiwen Gao <yiwen.gao@columbia.edu>
2020-12-23 23:24:25 +0000

    test

---
+++ src/network/client/recvmsg.cpp
@@ -0,0 +1,33 @@
+#include "conn.h"
+
+using namespace std;
+
+const char *CA_CERT = "";
+const char *CLIENT_CERT = "";
+const char *CLIENT_KEY = "";
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		cerr << "sendmsg: missing username" << endl;
+		exit(1);
+	}
+
+    string username = argv[1];
+    ClientConnection conn = ClientConnection(CA_CERT, CLIENT_CERT, CLIENT_KEY);
+    
+    // comm with server
+    RecvMsgReq req = RecvMsgReq(username);
+	conn.send(req.get_http_content());
+    string http_content = conn.recv();
+    MailResp resp = MailResp(http_content);
+
+    // output mail
+    cout << resp.sender << endl;
+    for (auto it = resp.receivers.begin(); it != resp.receivers.end(); ++it) {
+        cout << *it << endl;
+    }
+    // TODO decrypt msg!
+    cout << resp.msg << endl;
+
+	return 0;
+}
