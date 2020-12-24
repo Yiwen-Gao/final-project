e485a5643724308e588a33bddfdab1f1062c3be8
Yiwen Gao <yiwen.gao@columbia.edu>
2020-12-24 20:27:22 +0000

    add starter for decryption

--- src/network/client/recvmsg.cpp
+++ src/network/client/recvmsg.cpp
@@ -1,10 +1,95 @@
 #include "conn.h"
+#include <fstream>
+extern "C" {
+    #include <openssl/pem.h>
+    #include <openssl/cms.h>
+    #include <openssl/err.h>
+}
 
 using namespace std;
 
 const char *CA_CERT = "./trusted_certs/ca-chain.cert.pem";
 const char *CLIENT_CERT = "./dummy/cert.pem";
 const char *CLIENT_KEY = "./dummy/key.pem";
+
+const string input_path = "./";
+const string output_path = "";
+
+int decrypt() {
+    BIO *in = NULL, *out = NULL, *tbio = NULL;
+    X509 *rcert = NULL;
+    EVP_PKEY *rkey = NULL;
+    CMS_ContentInfo *cms = NULL;
+    int ret = 1;
+
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    /* Read in recipient certificate and private key */
+    tbio = BIO_new_file(CLIENT_KEY, "r");
+
+    if (!tbio)
+        goto err;
+
+    rcert = PEM_read_bio_X509(tbio, NULL, 0, NULL);
+    BIO_reset(tbio);
+    rkey = PEM_read_bio_PrivateKey(tbio, NULL, 0, NULL);
+
+    if (!rcert || !rkey)
+        goto err;
+
+    /* Open S/MIME message to decrypt */
+    in = BIO_new_file(input_path.c_str(), "r");
+    if (!in)
+        goto err;
+
+    /* Parse message */
+    cms = SMIME_read_CMS(in, NULL);
+    if (!cms)
+        goto err;
+
+    out = BIO_new_file(output_path.c_str(), "w");
+    if (!out)
+        goto err;
+
+    /* Decrypt S/MIME message */
+    if (!CMS_decrypt(cms, rkey, rcert, NULL, out, 0))
+        goto err;
+
+    ret = 0;
+
+ err:
+    if (ret) {
+        fprintf(stderr, "Error Decrypting Data\n");
+        ERR_print_errors_fp(stderr);
+    }
+
+    CMS_ContentInfo_free(cms);
+    X509_free(rcert);
+    EVP_PKEY_free(rkey);
+    BIO_free(in);
+    BIO_free(out);
+    BIO_free(tbio);
+    return ret;
+}
+
+void write_to_file(string path, string text) {
+    ofstream output_file(path);
+    if (output_file.is_open()) {
+        output_file << text;
+        output_file.close();
+    }
+}
+
+string read_from_file(string path) {
+    string msg = "";
+    ifstream input_file(path);
+    if (input_file.is_open()) {
+        input_file >> msg;
+    }
+
+    return msg;
+}
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -26,8 +111,9 @@ int main(int argc, char *argv[]) {
     // output mail
     cout << "[mail content]" << endl;
     cout << resp.address << endl << endl;
-    // TODO decrypt msg!
-    cout << resp.msg;
+    
+    write_to_file(input_path, resp.msg);
+    cout << read_from_file(output_path);
 
 	return 0;
 }
