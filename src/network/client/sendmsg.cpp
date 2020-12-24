57420cee120e411c89caf1b218df2ea7d83318fe
Hollis Lehv <hlehv@aol.com>
2020-12-24 18:31:22 +0000

    Started encryption

--- src/network/client/sendmsg.cpp
+++ src/network/client/sendmsg.cpp
@@ -1,5 +1,9 @@
 #include "conn.h"
 
+#include <openssl/pem.h>
+#include <openssl/cms.h>
+#include <openssl/err.h>
+
 using namespace std;
 
 const char *CA_CERT = "./trusted_certs/ca-chain.cert.pem";
@@ -31,6 +35,22 @@ int main(int argc, char *argv[]) {
 	for (auto it = resp.certs.begin(); it != resp.certs.end(); ++it) {
 		string cert = *it;
 		// TODO encrypt msg
+		BIO *in = NULL, *out = NULL, *tbio = NULL;
+		X509 *rcert = NULL;
+		STACK_OF(X509) *recips = NULL;
+		CMS_ContentInfo *cms = NULL;
+		int ret = 1;
+
+		int flags = CMS_STREAM;
+		
+		OpenSSL_add_all_algorithms();
+		ERR_load_crypto_strings();
+
+		tbio = BIO_new_file("signer.pem", "r");
+		if (!tbio)
+			goto err;
+
+
 		msgs.push_back(msg);
 	}
 	SendMsgMailReq mail_req = SendMsgMailReq(msgs);
