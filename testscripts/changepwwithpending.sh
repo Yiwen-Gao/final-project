f9dd37214402349d639425003e91c90a19187ade
Hollis Lehv <hlehv@aol.com>
2020-12-25 03:38:45 +0000

    Added some test scripts

--- testscripts/changepwwithpending.sh
+++ testscripts/changepwwithpending.sh
@@ -2,35 +2,27 @@
 cd ..
 rm -rf mailsystem
 
-#set up the mail system
+echo "Testing if changepw rejects if a user has pending mail"
 echo "Setting up mail system"
 ./setupmailsystem.sh mailsystem > testscripts/setupout.txt 2>&1
 cd mailsystem/server
 
-# run the server in the backgrouund
+# run the server in the background
 echo "Starting server"
 sudo ./server > ../../testscripts/serveroutput.txt 2>&1 &
 
 #run getcert in the client
 echo "Running getcert for muermo - enter grovelling_turtle when prompted"
-cd ../client
-./bin/getcert muermo > ../../testscripts/clientoutput.txt 2>&1 
+./bin/getcert muermo > ../../testscripts/clientoutput.txt 2>&1
 
-echo "Running getcert for repine - enter tallyho's_courage when prompted"
+o "Running getcert for repine - enter tallyho's_courage when prompted"
 ./bin/getcert repine >> ../../testscripts/clientoutput.txt 2>&1
 
 echo "Running getcert for addleness - enter Cardin_pwns when prompted"
 ./bin/getcert addleness >> ../../testscripts/clientoutput.txt 2>&1
 
-#here would want to verify that it ran correctly?
 echo "Running sendmsg"
 ./bin/sendmsg muermo < ../../testscripts/basicsendmsginput.txt >> ../../testscripts/clientoutput.txt 2>&1
 
-echo "Running recvmsg"
-./bin/recvmsg addleness >> ../../testscripts/clientoutput.txt 2>&1
-
-echo "Running changepw"
-./bin/changepw muermo >> ../../testscripts/clientoutput.txt 2>&1
-
-cd ../../testscripts
-rm setupout.txt clientoutput.txt serveroutput.txt
+echo "addleness trying to changepw despite having a pending message. Enter Cardin_pwns then newpassword when prompted"
+./bin/changepw addleness >> ../../testscript/clientoutput.txt 2>&1
